9674b72a09a3d3d7ed1bcc63b004bf6f75316cd7
Mike-Dai <mrmikedai@outlook.com>
2019-07-14 09:07:44 +0800

    Finish Q17: Print 1 to max n digits

---
+++ q17-PrintOneToN.cpp
@@ -0,0 +1,56 @@
+/* 
+ * official answer
+ */
+void Print1ToMaxOfNDigits(int n) {
+	if (n <= 0) {
+		return;
+	}
+	char* number = new char[n + 1];
+	memset(number, '0', n);
+	number[n] = '\0';
+
+	while (!Increment(number)) {
+		PrintNumber(number);
+	}
+
+	delete[] number;
+}
+
+bool Increment(char* number) {
+	bool isOverFlow = false;
+	int nTakeOver = 0;
+	int length = strlen(number);
+	for (int i = length - 1; i >= 0; --i) 
+	{
+		sum = number[i] - '0' + nTakeOver;
+		if (i == length - 1) {
+			sum++;
+		}
+		if (sum >= 10) {
+			if (i == 0) {
+				isOverFlow = true;
+			}
+			nTakeOver = 1;
+			sum -= 10;
+			number[i] = '0' + sum;
+		}
+		else {
+			number[lenght] = '0' + sum;
+			break;
+		}
+	}
+	return isOverFlow;
+}
+
+void PrintNumber(char* number) {
+	bool isBeginning0 = true;
+	for (int i = 0; number[i] != '\0'; ++i) {
+		if (isBeginning0 && number[i] != '0') {
+			isBeginning0 = false;
+		}
+		if (!isBeginning0) {
+			printf("%c", number[i]);
+		}
+	}
+	printf("\t");
+}
