b811a4290eaf3b947068856ee13e6156141d12d1
Mike-Dai <mrmikedai@outlook.com>
2019-07-14 13:38:36 +0800

    Add recursively solution for Q17

--- q17-PrintOneToN.cpp
+++ q17-PrintOneToN.cpp
@@ -1,5 +1,5 @@
 /* 
- * official answer
+ * official answer1
  */
 void Print1ToMaxOfNDigits(int n) {
 	if (n <= 0) {
@@ -53,4 +53,31 @@ void PrintNumber(char* number) {
 		}
 	}
 	printf("\t");
+}
+
+/*
+ * official answer2
+ */
+void Print1ToMaxOfNDigits(int n) {
+	if (n <= 0) {
+		return;
+	}
+	char* number = new char[n + 1];
+	number[n] = '\0';
+	for (int i = 0; i < 10; ++i) {
+		number[0] = '0' + i;
+		Print1ToMaxOfNDigitsRecursively(number, n, 0);
+	}
+	delete[] number;
+}
+
+Print1ToMaxOfNDigitsRecursively(char* number, int length, int index) {
+	if (index == length - 1) {
+		PrintNumber(number);
+		return;
+	}
+	for (int i = 0; i < 10; ++i) {
+		number[index + 1] = '0' + i;
+		Print1ToMaxOfNDigitsRecursively(number, length, index + 1);
+	}
 }
