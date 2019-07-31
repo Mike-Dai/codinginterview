8bffe81384ec55da712dd81587e02b81c180f52a
Mike-Dai <mrmikedai@outlook.com>
2019-07-31 09:32:33 +0800

    Finish Q36: Convert binary search tree

---
+++ q36-ConvertBinarySearchTree.cpp
@@ -0,0 +1,32 @@
+/*
+ * official answer
+ * hard
+ */
+BinaryTreeNode* Convert(BinaryTreeNode* pRootOfTree) {
+	BinaryTreeNode* pLastNodeInList = nullptr;
+	ConvertNode(pRootOfTree, &pLastNodeInList);
+
+	BinaryTreeNode* pHeadOfList = pLastNodeInList;
+	while (pHeadOfList != nullptr && pHeadOfList->m_pLeft != nullptr) {
+		pHeadOfList = pHeadOfList->m_pLeft;
+	}
+	return pHeadOfList;
+}
+
+void ConvertNode(BinaryTreeNode* pNode, BinaryTreeNode** pLastNodeInList) {
+	if (pNode == nullptr) {
+		return;
+	}
+	BinaryTreeNode* pCurrent = pNode;
+	if (pCurrent->m_pLeft != nullptr) {
+		ConvertNode(pCurrent->m_pLeft, pLastNodeInList);
+	}
+	pCurrent->m_pLeft = *pLastNodeInList;
+	if (*pLastNodeInList != nullptr) {
+		(*pLastNodeInList)->m_pRight = pCurrent;
+	}
+	*pLastNodeInList = pCurrent;
+	if (pCurrent->m_pRight != nullptr) {
+		ConvertNode(pCurrent->m_pRight, pLastNodeInList);
+	}
+}
